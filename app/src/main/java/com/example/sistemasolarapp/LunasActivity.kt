76522c4bf84cce90ac328ee96e095492a70d1ca7
HARUSITA <160783729+HARUSITA@users.noreply.github.com>
2026-03-02 23:04:38 -0600

    Interfaz lunas

--- app/src/main/java/com/example/sistemasolarapp/LunasActivity.kt
+++ app/src/main/java/com/example/sistemasolarapp/LunasActivity.kt
@@ -1,6 +1,8 @@
 package com.example.sistemasolarapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LunasActivity : AppCompatActivity() {
@@ -8,5 +10,21 @@ class LunasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lunas)
+
+        // El código del botón DEBE ir aquí dentro
+        val btnGalaxia = findViewById<Button>(R.id.btnBackToPlanets)
+
+        btnGalaxia.setOnClickListener {
+            // Creamos el salto a GalaxiaActivity
+            val intent = Intent(this, GalaxiaActivity::class.java)
+
+            // Estas banderas limpian la pila de actividades
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+
+            startActivity(intent)
+
+            // Finalizamos esta actividad para que no se pueda volver atrás a las lunas
+            finish()
+        }
     }
 }
